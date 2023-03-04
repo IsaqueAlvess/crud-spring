6f8c0351c740093e092abd9c62cde633055b840b
Isaque_Alvess <ysak.alves@outlook.com>
2023-03-04 19:24:22 -0300

    Appling the POST method
    Aplicando o método post e retorno.

--- src/main/java/com/isaque/crudspring/controller/CourseController.java
+++ src/main/java/com/isaque/crudspring/controller/CourseController.java
@@ -2,8 +2,14 @@ package com.isaque.crudspring.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.isaque.crudspring.model.Course;
@@ -24,4 +30,12 @@ public class CourseController {
         return courseRepository.findAll();
     }
     
+    @PostMapping        //Anotação que substitui @RequestMappaing(method = RequestMethod.POST)
+    @ResponseStatus(code = HttpStatus.CREATED )
+    public Course create(@RequestBody Course course){
+        //System.out.println(course.getName());
+        return courseRepository.save(course);
+        // return ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course));
+    }
+
 }
