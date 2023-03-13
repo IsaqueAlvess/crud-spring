b2f7af896d31be77174a9c1d592a67175d22b0b6
Isaque_Alvess <ysak.alves@outlook.com>
2023-03-12 21:52:41 -0300

    updating PutMapping - adding Delete

--- src/main/java/com/isaque/crudspring/controller/CourseController.java
+++ src/main/java/com/isaque/crudspring/controller/CourseController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,8 @@ import com.isaque.crudspring.model.Course;
 import com.isaque.crudspring.repository.CourseRepository;
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 @RestController
 @RequestMapping("/api/courses")
@@ -43,6 +46,32 @@ public class CourseController {
         //System.out.println(course.getName());
         return courseRepository.save(course);
         // return ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course));
+    }
+
+
+    @PutMapping(value="/{id}")
+    @ResponseStatus(code = HttpStatus.OK)
+    public ResponseEntity <Course> update(@PathVariable Long id, @RequestBody Course course) {
+        
+        return courseRepository.findById(id)
+                .map(recordFound -> {
+                    recordFound.setName(course.getName());
+                    recordFound.setCategory(course.getCategory());
+                    Course updated = courseRepository.save(recordFound);
+                    return ResponseEntity.ok().body(updated);
+                })
+                .orElse(ResponseEntity.notFound().build());
+
+    }
+
+    @DeleteMapping(value="/{id}")
+    public ResponseEntity <Void> delete(@PathVariable Long id){
+        return courseRepository.findById(id)
+            .map(recordFound -> {
+                courseRepository.deleteById(id);
+                return ResponseEntity.noContent().<Void>build();
+            })
+            .orElse(ResponseEntity.notFound().build());        
     }
 
 }
