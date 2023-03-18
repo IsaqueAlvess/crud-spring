e334478689c983a4bcdf9f94b80df4b2771ea0bf
Isaque_Alvess <ysak.alves@outlook.com>
2023-03-17 22:17:52 -0300

    Creating an service class
    removing the connection with the repositoryClass directly from the Controller and creating the service class

--- src/main/java/com/isaque/crudspring/controller/CourseController.java
+++ src/main/java/com/isaque/crudspring/controller/CourseController.java
@@ -11,71 +11,66 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.isaque.crudspring.model.Course;
-import com.isaque.crudspring.repository.CourseRepository;
+import com.isaque.crudspring.service.CourseService;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
-import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PutMapping;
 
 @Validated
 @RestController
 @RequestMapping("/api/courses")
-@AllArgsConstructor
 public class CourseController {
     
 
-    private final CourseRepository courseRepository;
-        
+    private final CourseService courseService;
+    
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
     @GetMapping
-    public List<Course> list() {
-        return courseRepository.findAll();
+    public @ResponseBody List<Course> list() {
+        return courseService.list();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity <Course> findById(@PathVariable @NotNull @Positive Long id){
-        return courseRepository.findById(id)
+        return courseService.findById(id)
                 .map(rec -> ResponseEntity.ok().body(rec))
                 .orElse(ResponseEntity.notFound().build());
     }
     
-    @PostMapping        //Anotação que substitui @RequestMappaing(method = RequestMethod.POST)
+    @PostMapping                    //Anotação que substitui @RequestMappaing(method = RequestMethod.POST)
     @ResponseStatus(code = HttpStatus.CREATED )
     public Course create(@RequestBody @Valid Course course){
-        //System.out.println(course.getName());
-        return courseRepository.save(course);
-        // return ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course));
+        
+        return courseService.create(course);
+        
     }
 
 
     @PutMapping(value="/{id}")
     @ResponseStatus(code = HttpStatus.OK)
-    public ResponseEntity <Course> update(@PathVariable Long id, @RequestBody @Positive Course course) {
+    public ResponseEntity <Course> update(@NotNull @PathVariable Long id, @RequestBody @Valid Course course) {
         
-        return courseRepository.findById(id)
-                .map(recordFound -> {
-                    recordFound.setName(course.getName());
-                    recordFound.setCategory(course.getCategory());
-                    Course updated = courseRepository.save(recordFound);
-                    return ResponseEntity.ok().body(updated);
-                })
+        return courseService.update(id, course)
+                .map(recordFound -> ResponseEntity.ok().body(recordFound))
                 .orElse(ResponseEntity.notFound().build());
-
     }
     //Hard Delete - remoção física do Banco de Dados
     @DeleteMapping(value="/{id}")
-    public ResponseEntity <Void> delete(@PathVariable @NotNull @Positive Long id){
-        return courseRepository.findById(id)
-            .map(recordFound -> {
-                courseRepository.deleteById(id);
-                return ResponseEntity.noContent().<Void>build();
-            })
-            .orElse(ResponseEntity.notFound().build());        
+    public ResponseEntity<Void> delete(@PathVariable @NotNull @Positive Long id){
+        if(courseService.delete(id)){
+            return ResponseEntity.noContent().<Void>build();
+        }
+        return ResponseEntity.notFound().build();
     }
 
     
